1092b23ce1625820dc30c875560ce219aff70ce0
mdao2211 <superhero21442922@gmail.com>
2024-01-17 15:04:10 +0700

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ProductList from './ProductList';
+import Records from './records.json';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState(Records);
+  const [filteredProducts, setFilteredProducts] = useState(Records);
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
+  const handleFilter = (value) => {
+    if (value === 'All') {
+      setFilteredProducts(products);
+    } else {
+      const filtered = products.filter((product) =>
+        product.content.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
+  };
+
+  const handleEdit = (productId, editedContent) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === productId ? { ...product, content: editedContent } : product
+      )
+    );
+  };
+
+  const handleDelete = (productId) => {
+    setProducts((prevProducts) => prevProducts.filter((product) => product.id !== productId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => handleFilter('All')}>Show All</button>
+      <button onClick={() => handleFilter('Mer')}>Filter Mer</button>
+      <button onClick={() => handleFilter('Porsche')}>Filter Porsche</button>
+      <button onClick={() => handleFilter('Lambor')}>Filter Lambor</button>
+
+      <ProductList products={filteredProducts} onEdit={handleEdit} onDelete={handleDelete} />
     </div>
   );
 }
